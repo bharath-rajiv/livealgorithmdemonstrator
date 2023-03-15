16fb42726b5f1cedafc97cd9917d448b4b3a558c
=bharath-rajiv <bharathrajiva.aids2020@citchennai.net>
2023-03-15 09:12:41 +0530

    15 march 23

--- src/components/MenuData.js
+++ src/components/MenuData.js
@@ -1,7 +1,7 @@
 export const MenuData = [
-    { title: 'Home',url:"/", cName: 'nav-links', icon: "fa-solid fa-house-user" },
-    { title: 'Search',url:"/search", cName: 'nav-links', icon: "fa-solid fa-search" },
-    { title: 'Sort',url:"/sort", cName: 'nav-links', icon: "fa-solid fa-sort" },
-    { title: 'Graph',url:"/graph", cName: 'nav-links', icon: "fa-solid fa-network-wired" },
-    { title: 'Login/Signup',url:"/login", cName: 'nav-links', icon: "fa-solid fa-user" }
+    { title: 'Home',url:"/livealgorithmdemonstrator", cName: 'nav-links', icon: "fa-solid fa-house-user" },
+    { title: 'Search',url:"/livealgorithmdemonstrator/search", cName: 'nav-links', icon: "fa-solid fa-search" },
+    { title: 'Sort',url:"/livealgorithmdemonstrator/sort", cName: 'nav-links', icon: "fa-solid fa-sort" },
+    { title: 'Graph',url:"/livealgorithmdemonstrator/graph", cName: 'nav-links', icon: "fa-solid fa-network-wired" },
+    { title: 'Login/Signup',url:"/livealgorithmdemonstrator/login", cName: 'nav-links', icon: "fa-solid fa-user" }
 ];
